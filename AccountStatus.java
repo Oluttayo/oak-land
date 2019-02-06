da51db70d4440603c690327e0b14cd234bc8a46c
Oluttayo <47345935+Oluttayo@users.noreply.github.com>
2019-02-06 23:58:24 +0100

    Add files via upload

---
+++ AccountStatus.java
@@ -0,0 +1,16 @@
+/**
+@author : Tayo
+@email : 
+@date : 06-FEB-2019
+@purpose : To state the account type
+
+*/
+
+package account;
+
+public enum AccountStatus{
+	
+	ACTIVE,
+	INACTIVE;
+	
+}
---
+++ account.java
@@ -0,0 +1,19 @@
+/**
+@author : Tayo
+@email : 
+@date : 06-FEB-2019
+@purpose : To list account attributes in software model
+
+*/
+
+package account;
+
+public class account{
+	private String accountName
+	private AccountType accountType
+	private String accountNumber
+	private String accountBalance
+	private AccountStatus accountStatus
+	private String dateOpened
+	
+}
