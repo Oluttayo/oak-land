abf1b230790d1bc123408aedf5e6e30691f2d696
Oluttayo <47345935+Oluttayo@users.noreply.github.com>
2019-02-06 23:59:29 +0100

    Add files via upload

---
+++ GenderType.java
@@ -0,0 +1,17 @@
+/**
+@author : Tayo
+@email
+@date : 06-FEB-2019
+@purpose : To state the user's gender
+
+*/
+
+package user;
+
+public enum GenderType{
+	
+	MALE,
+	FEMALE;
+	
+}
+
---
+++ User.java
@@ -0,0 +1,20 @@
+/**
+@author : Tayo
+@email
+@date : 06-FEB-2019
+@purpose : To list the user's attributes
+
+*/
+
+package user;
+
+public class user {
+
+	private String fullName
+	private String email
+	private String mobileNumber
+	private GenderType gender
+	private UserStatus userStatus
+	private UserType userType
+
+}
