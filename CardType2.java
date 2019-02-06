2be0eac539b0b6c0586165e82f3b4009497301d2
Oluttayo <47345935+Oluttayo@users.noreply.github.com>
2019-02-06 23:44:56 +0100

    Card
    Card Details

---
+++ CardType2.java
@@ -0,0 +1,17 @@
+/**
+@author : Tayo
+@email : 
+@date : 06-FEB-2019
+@purpose : To list the attributes of user's bank card
+
+*/
+
+package account.card;
+
+public enum CardType2{
+	
+	MASTERCARD,
+	VISA,
+	VERVE;
+	
+}
---
+++ card.java
@@ -0,0 +1,23 @@
+/**
+@author : Tayo
+@email : 
+@date : 06-FEB-2019
+@purpose : To list the attributes of user's bank card
+
+*/
+
+
+package account;
+
+public class account{
+	
+	private String nameOnCard
+	private CardType cardType
+	private CardType2 cardType2
+	private String cardNumber
+	private String cardExpiryDate
+	private int cardCVV
+	private String dateCollected
+	
+	
+}
